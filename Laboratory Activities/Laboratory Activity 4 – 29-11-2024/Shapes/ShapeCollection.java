fb073da7efc6a6af3f4497661ed65e3c90ca3738
jimcsy <165743267+jimcsy@users.noreply.github.com>
2024-11-29 15:42:31 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 4 – 29-11-2024/Shapes/ShapeCollection.java
@@ -0,0 +1,16 @@
+public class ShapeCollection {
+    private Shape [] shapes;
+
+    ShapeCollection(Shape [] shapes){
+        this.shapes = shapes;
+    }
+
+    public void getPropertyValues(){
+        for (Shape shape : shapes){
+            System.out.println(shape.printShapeType());
+            System.out.println(String.format("Area: %.2f", shape.getArea()));
+            System.out.println(String.format("Perimeter: %.2f", shape.getPerimeter()) + "\n");
+        }
+    }
+    
+}
---
+++ Laboratory Activities/Laboratory Activity 4 – 29-11-2024/Shapes/Triangle.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+public class Triangle implements Shape{
+    private double a, b, c;
+
+    Triangle (double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public double getArea(){
+        double s = (a + b + c)/2;
+        return Math.sqrt(s*(s-a)*(s-b)*(s-c) );
+    }
+
+    public double getPerimeter(){
+        return (a + b + c);
+    }
+
+    public String printShapeType(){
+        return "Triangle";
+    }
+}
